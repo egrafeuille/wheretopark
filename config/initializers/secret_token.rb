14ea02c6e59fdcc5130e924888fa856ca3a6b37a
egrafeuille <ernesto.grafeuille@gmail.com>
2014-09-30 15:32:31 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wheretopark::Application.config.secret_token = '6553f20eb870b412c626a8b8b1cde50a25df85f7f1c3a68bae211af5c4c7ba362cfccda91f4d4f54d2ac8466bf764b7ac17fe8db73641b4f97ffa362f9de9a9f'
