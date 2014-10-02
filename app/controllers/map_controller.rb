516ed6f844fe6984fe923da1187444e4b1775c04
egrafeuille <ernesto.grafeuille@gmail.com>
2014-10-02 16:38:01 -0300

    Marker customizado

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,10 +1,16 @@
 class MapController < ApplicationController
   def index
+    @icon = {"url"=> "/images/marker0111.png",
+        "width"=> 45,
+        "height"=> 70
+        }
+#    ''
     @parkings = Parking.all
     @hash = Gmaps4rails.build_markers(@parkings) do |parking, marker|
       marker.lat parking.latitude
       marker.lng parking.longitude
-      marker.infowindow parking.name
+      marker.infowindow parking.hour_fee
+      marker.picture @icon 
     end
   end
 end
